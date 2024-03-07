d823f897fc7cba7a9249e00894356faccba6e9d3
schreifuchs <kontakt@schreifuchs.ch>
2024-03-07 15:00:07 +0100

    frontend

---
+++ client/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+  export const text: string = "button";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button class="p-1 hover:bg-gray-100 border-black border-2 rounded" on:click={() => dispatch('click')}>
+  <slot />
+</button>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import Button from "$lib/components/Button.svelte";
+
+    let uuid: string = "";
+
+</script>
+<section class="flex h-full w-full flex-col gap-4 p-4">
+  <Button on:click={() => goto("/game")}>New Game</Button>
+  <div class="w-full flex flex-row gap-4">
+    <input type="text" bind:value={uuid} class="border-2 p-1 border-black rounded justify-between grow " /> 
+    <Button on:click={() => goto(`/game?uuid=${uuid}`)}>join</Button>
+  </div>
+  
+</section>
